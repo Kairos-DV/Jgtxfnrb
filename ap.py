d371085f17bef1b1ac30b225dd4f3ccab1d48beb
Karchenko <Karchenko@gmail.com>
2025-03-15 15:21:31 +0500

    2

--- ap.py
+++ ap.py
@@ -1,1 +1,12 @@
 print(9)
+print("Привет ФЕДЯ")
+otv="пусто"
+while otv != "Отстань":
+    print("Ты покакал?")
+    otv = input ()
+    if otv == "Да" or otv == "да":
+        print("Я рад")
+    elif otv == "Нет" or otv == "нет":
+        print("А надо было покакать")
+    else:
+        print("Не понял")
