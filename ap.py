82e52175cbd8555cd762d876775adb214d147c31
Karchenko <Karchenko@gmail.com>
2025-03-16 14:57:43 +0500

    1

--- ap.py
+++ ap.py
@@ -1,12 +1,3 @@
-print(9)
-print("Даров ФЕДЯ")
-otv="пусто"
-while otv != "Отстань":
-    print("Ты какал""?")
-    otv = input ()
-    if otv == "Да" or otv == "Yes":
-        print("Я рад")
-    elif otv == "Нет" or otv == "No":
-        print("А надо было покакать")
-    else:
-        print("Не понял")
+import pandas as pd
+df_orders = pd.read_excel('C:\PROJECT\Jgtxfnrb2\Excel_Data\Левый.xlsx')
+print(df_orders.head())
